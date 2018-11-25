1120b32c720ac9ac2655cde3bdc33847e6a887cb
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-25 03:57:29 -0300

    Add basic gui stuff for client.py

--- client.py
+++ client.py
@@ -1,6 +1,15 @@
+# coding: utf-8
+
+# standard library
 import protocol
 import socket
+import sys
 from datetime import datetime
+
+# PyQt5 imports
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5 import uic
 
 
 class Client(object):
@@ -20,9 +29,11 @@ class Client(object):
         )
 
     def connect(self):
+        """Conecta com o servidor"""
         return self.socket.connect((self.host, self.port))
 
     def send_message(self, message, subject=None):
+        """Envia uma mensagem para o servidor"""
         m = protocol.Message(
             self.name,
             subject,
@@ -32,13 +43,39 @@ class Client(object):
         m.send(self.socket)
 
 
+class ClientGUI(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/client.ui', self)
+        self.shortcut = QtWidgets.QShortcut(
+            QtGui.QKeySequence("Ctrl+Enter"),  # send message shortcut
+            self
+        )
+        self.shortcut.activated.connect(self.send)
+        self.send_button.clicked.connect(self.send)
+        self.client = Client()
+
+    def send(self):
+        subject = self.subject_text.text()
+        message = self.message_text.toPlainText()
+        name = self.name_text.text()
+        self.message_text.clear()
+        self.client.name = name
+        if message:
+            self.client.send_message(message, subject)
+
+    @classmethod
+    def run(cls):
+        app = QtWidgets.QApplication(sys.argv)
+        main = cls()
+        main.show()
+        main.client.connect()
+        msg = "Cliente conectado a {}:{}".format(main.client.host,
+                                                 main.client.port)
+        main.statusBar().showMessage(msg)
+        sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-    c = Client()
-    c.connect()
-    print("Cliente conectado a {}:{}".format(c.host, c.port))
-    while True:
-        try:
-            c.send_message(input('>>> '))
-        except:
-            break
-    c.socket.close()
+    ClientGUI.run()
